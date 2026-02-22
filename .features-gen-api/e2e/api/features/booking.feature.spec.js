c221bfbfd554e96172ba353326dd30e46c88ab55
niceact00 <jervis.socmedandgames110@gmail.com>
2026-02-22 20:38:22 +0800

    added settings.json to fix undefined steps in feature files

--- .features-gen-api/e2e/api/features/booking.feature.spec.js
+++ .features-gen-api/e2e/api/features/booking.feature.spec.js
@@ -3,24 +3,24 @@ import { test } from "playwright-bdd";
 
 test.describe('Booking API', () => {
 
-  test('Create Booking', async ({ When, Then, request }) => { 
+  test('Create Booking', { tag: ['@api'] }, async ({ When, Then, request }) => { 
     await When('I create a new booking', null, { request }); 
     await Then('the booking should be created successfully'); 
   });
 
-  test('Get Booking', async ({ Given, When, Then, request }) => { 
+  test('Get Booking', { tag: ['@api'] }, async ({ Given, When, Then, request }) => { 
     await Given('I have an existing booking', null, { request }); 
     await When('I retrieve the booking', null, { request }); 
     await Then('the booking details should be returned'); 
   });
 
-  test('Update Booking', async ({ Given, When, Then, request }) => { 
+  test('Update Booking', { tag: ['@api'] }, async ({ Given, When, Then, request }) => { 
     await Given('I have a booking and auth token', null, { request }); 
     await When('I update the booking', null, { request }); 
     await Then('the booking should be updated successfully'); 
   });
 
-  test('Delete Booking', async ({ Given, When, Then, request }) => { 
+  test('Delete Booking', { tag: ['@api'] }, async ({ Given, When, Then, request }) => { 
     await Given('I have a booking and auth token', null, { request }); 
     await When('I delete the booking', null, { request }); 
     await Then('the booking should be deleted successfully'); 
@@ -37,8 +37,8 @@ test.use({
 });
 
 const bddFileData = [ // bdd-data-start
-  {"pwTestLine":6,"pickleLine":3,"tags":[],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Action","textWithKeyword":"When I create a new booking","stepMatchArguments":[]},{"pwStepLine":8,"gherkinStepLine":5,"keywordType":"Outcome","textWithKeyword":"Then the booking should be created successfully","stepMatchArguments":[]}]},
-  {"pwTestLine":11,"pickleLine":7,"tags":[],"steps":[{"pwStepLine":12,"gherkinStepLine":8,"keywordType":"Context","textWithKeyword":"Given I have an existing booking","stepMatchArguments":[]},{"pwStepLine":13,"gherkinStepLine":9,"keywordType":"Action","textWithKeyword":"When I retrieve the booking","stepMatchArguments":[]},{"pwStepLine":14,"gherkinStepLine":10,"keywordType":"Outcome","textWithKeyword":"Then the booking details should be returned","stepMatchArguments":[]}]},
-  {"pwTestLine":17,"pickleLine":12,"tags":[],"steps":[{"pwStepLine":18,"gherkinStepLine":13,"keywordType":"Context","textWithKeyword":"Given I have a booking and auth token","stepMatchArguments":[]},{"pwStepLine":19,"gherkinStepLine":14,"keywordType":"Action","textWithKeyword":"When I update the booking","stepMatchArguments":[]},{"pwStepLine":20,"gherkinStepLine":15,"keywordType":"Outcome","textWithKeyword":"Then the booking should be updated successfully","stepMatchArguments":[]}]},
-  {"pwTestLine":23,"pickleLine":17,"tags":[],"steps":[{"pwStepLine":24,"gherkinStepLine":18,"keywordType":"Context","textWithKeyword":"Given I have a booking and auth token","stepMatchArguments":[]},{"pwStepLine":25,"gherkinStepLine":19,"keywordType":"Action","textWithKeyword":"When I delete the booking","stepMatchArguments":[]},{"pwStepLine":26,"gherkinStepLine":20,"keywordType":"Outcome","textWithKeyword":"Then the booking should be deleted successfully","stepMatchArguments":[]}]},
+  {"pwTestLine":6,"pickleLine":4,"tags":["@api"],"steps":[{"pwStepLine":7,"gherkinStepLine":5,"keywordType":"Action","textWithKeyword":"When I create a new booking","stepMatchArguments":[]},{"pwStepLine":8,"gherkinStepLine":6,"keywordType":"Outcome","textWithKeyword":"Then the booking should be created successfully","stepMatchArguments":[]}]},
+  {"pwTestLine":11,"pickleLine":8,"tags":["@api"],"steps":[{"pwStepLine":12,"gherkinStepLine":9,"keywordType":"Context","textWithKeyword":"Given I have an existing booking","stepMatchArguments":[]},{"pwStepLine":13,"gherkinStepLine":10,"keywordType":"Action","textWithKeyword":"When I retrieve the booking","stepMatchArguments":[]},{"pwStepLine":14,"gherkinStepLine":11,"keywordType":"Outcome","textWithKeyword":"Then the booking details should be returned","stepMatchArguments":[]}]},
+  {"pwTestLine":17,"pickleLine":13,"tags":["@api"],"steps":[{"pwStepLine":18,"gherkinStepLine":14,"keywordType":"Context","textWithKeyword":"Given I have a booking and auth token","stepMatchArguments":[]},{"pwStepLine":19,"gherkinStepLine":15,"keywordType":"Action","textWithKeyword":"When I update the booking","stepMatchArguments":[]},{"pwStepLine":20,"gherkinStepLine":16,"keywordType":"Outcome","textWithKeyword":"Then the booking should be updated successfully","stepMatchArguments":[]}]},
+  {"pwTestLine":23,"pickleLine":18,"tags":["@api"],"steps":[{"pwStepLine":24,"gherkinStepLine":19,"keywordType":"Context","textWithKeyword":"Given I have a booking and auth token","stepMatchArguments":[]},{"pwStepLine":25,"gherkinStepLine":20,"keywordType":"Action","textWithKeyword":"When I delete the booking","stepMatchArguments":[]},{"pwStepLine":26,"gherkinStepLine":21,"keywordType":"Outcome","textWithKeyword":"Then the booking should be deleted successfully","stepMatchArguments":[]}]},
 ]; // bdd-data-end
